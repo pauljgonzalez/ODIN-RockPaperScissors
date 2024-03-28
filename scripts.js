bc5743452e5f4cb75a29b38c45b0b4a402588ba8
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-03-28 12:53:55 -0400

    Feat: Add update Scores and results
    Add scores update based on winner
    Add winner logic for paper

--- scripts.js
+++ scripts.js
@@ -1,25 +1,55 @@
 const cpuChoice = ["ROCK", "PAPER", "SCISSORS"];
+
 let playerPick = "";
+let playerScoreCounter = 0;
+let cpuScoreCounter = 0;
+let winner = "";
+const cpuScore = document.querySelector(".cpu-score");
+const playerScore = document.querySelector(".player-score");
 
 function getComputerChoice(){
 let number = Math.floor(Math.random() * 3);
 return number;
 }
-cpuPick = cpuChoice[getComputerChoice()];
 
  document.querySelector("button.rock").onclick = function() {
     playerPick = "ROCK";
-    console.log(playerPick);
+    cpuPick = cpuChoice[getComputerChoice()];
+    results(cpuPick,playerPick);
+    cpuScore.innerHTML = cpuScoreCounter;
+    playerScore.innerHTML = playerScoreCounter;
+    
 }
 document.querySelector("button.paper").onclick = function() {
     playerPick = "PAPER";
-    console.log(playerPick);
+    cpuPick = cpuChoice[getComputerChoice()];
+    results(cpuPick,playerPick);
+    cpuScore.innerHTML = cpuScoreCounter;
+    playerScore.innerHTML = playerScoreCounter;
 }   
 document.querySelector("button.scissors").onclick = function() {
     playerPick = "SCISSORS";
-    console.log(playerPick);
+    cpuPick = cpuChoice[getComputerChoice()];
+    results(cpuPick,playerPick);
+    cpuScore.innerHTML = cpuScoreCounter;
+    playerScore.innerHTML = playerScoreCounter;
+}
+
+function results(cpu,player){
+    if( player === "PAPER" && cpu === "ROCK" ){
+        winner = "Player won with Paper";
+        playerScoreCounter ++;
+    }
+    else if( player === "PAPER" && cpu === "SCISSORS" ){
+        winner = "CPU won with Scissors";
+        cpuScoreCounter ++;
+    }
+    else if( player === "PAPER" && cpu === "PAPER" ){
+        winner = "Draw";
+    }
+
+
 }
 
 
 
-console.log(cpuPick);
