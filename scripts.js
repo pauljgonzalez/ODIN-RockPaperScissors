2678e498c54521ed0d3a10221b73c0b3e9b8405d
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-03-28 12:11:29 -0400

    FEAT: Add Scores and CPU HTML. Add button click Java
    Add scores and CPU side to HTML.
    Add onbutton click to store player choice
    Add random CPU choice

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
-console.log("test")
+const cpuChoice = ["ROCK", "PAPER", "SCISSORS"];
+let playerPick = "";
+
+function getComputerChoice(){
+let number = Math.floor(Math.random() * 3);
+return number;
+}
+cpuPick = cpuChoice[getComputerChoice()];
+
+ document.querySelector("button.rock").onclick = function() {
+    playerPick = "ROCK";
+    console.log(playerPick);
+}
+document.querySelector("button.paper").onclick = function() {
+    playerPick = "PAPER";
+    console.log(playerPick);
+}   
+document.querySelector("button.scissors").onclick = function() {
+    playerPick = "SCISSORS";
+    console.log(playerPick);
+}
+
+
+
+console.log(cpuPick);
