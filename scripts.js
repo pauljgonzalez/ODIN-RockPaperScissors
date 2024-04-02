82b984531f63b66ef7bf61a6d87022d0e99c5c53
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-04-02 10:42:46 -0400

    Refactor: Change winner logic
    Change winner logic to seperate code depending on
    player choice.
    
    Bug: Fix reset game bug
    
    Change logic to check for winner at end of selection
    So player did not have to click another option to check for winner

--- scripts.js
+++ scripts.js
@@ -21,33 +21,36 @@ return number;
  document.querySelector("button.rock").onclick = function() {
     playerPick = "ROCK";
     cpuPick = cpuChoice[getComputerChoice()];
-    results(cpuPick,playerPick);
+    resultsRock(cpuPick);
     cpuScore.innerHTML = cpuScoreCounter;
     playerScore.innerHTML = playerScoreCounter;
     winnerResults.innerHTML = winner;
     cpuChoiceText.innerHTML = cpuPick;
     playerChoice.innerHTML = playerPick;
+    checkWinner();
     
 }
 document.querySelector("button.paper").onclick = function() {
     playerPick = "PAPER";
     cpuPick = cpuChoice[getComputerChoice()];
-    results(cpuPick,playerPick);
+    resultsPaper(cpuPick);
     cpuScore.innerHTML = cpuScoreCounter;
     playerScore.innerHTML = playerScoreCounter;
     winnerResults.innerHTML = winner;
     cpuChoiceText.innerHTML = cpuPick
     playerChoice.innerHTML = playerPick;
+    checkWinner();
 }   
 document.querySelector("button.scissors").onclick = function() {
     playerPick = "SCISSORS";
     cpuPick = cpuChoice[getComputerChoice()];
-    results(cpuPick,playerPick);
+    resultsScissors(cpuPick);
     cpuScore.innerHTML = cpuScoreCounter;
     playerScore.innerHTML = playerScoreCounter;
     winnerResults.innerHTML = winner;
     cpuChoiceText.innerHTML = cpuPick
     playerChoice.innerHTML = playerPick;
+    checkWinner();
 }
 
 document.querySelector("button.reset-game").onclick = function(){
@@ -66,20 +69,21 @@ document.querySelector("button.reset-game").onclick = function(){
 
 }
 
-function results(cpu,player){
+
+function checkWinner(){
     if (cpuScoreCounter === 5){
-        winner = "CPU won the game";
-        resetGame.innerHTML = "Reset Game";
-        document.querySelector("button.reset-game").disabled = false;
-        document.querySelector("button.reset-game").hidden = false;
-        document.querySelector("button.rock").disabled = true;
-        document.querySelector("button.rock").hidden = true;
-        document.querySelector("button.paper").disabled = true;
-        document.querySelector("button.paper").hidden = true;
-        document.querySelector("button.scissors").disabled = true;
-        document.querySelector("button.scissors").hidden = true;
-    }
-    else if(playerScoreCounter === 5){
+    winner = "CPU won the game";
+    resetGame.innerHTML = "Reset Game";
+    document.querySelector("button.reset-game").disabled = false;
+    document.querySelector("button.reset-game").hidden = false;
+    document.querySelector("button.rock").disabled = true;
+    document.querySelector("button.rock").hidden = true;
+    document.querySelector("button.paper").disabled = true;
+    document.querySelector("button.paper").hidden = true;
+    document.querySelector("button.scissors").disabled = true;
+    document.querySelector("button.scissors").hidden = true;
+    
+    } else if(playerScoreCounter === 5){
         winner = "Player won the game";
         resetGame.innerHTML = "Reset Game";
         document.querySelector("button.reset-game").disabled = false;
@@ -91,37 +95,46 @@ function results(cpu,player){
         document.querySelector("button.scissors").disabled = true;
         document.querySelector("button.scissors").hidden = true;
     }
-    else if( player === "PAPER" && cpu === "ROCK" ){
+}
+
+function resultsRock(cpu){
+
+    if(cpu === "ROCK" ){
+        winner = "Draw";
+    
+    } else if(cpu === "SCISSORS" ){
+        winner = "Player won with Rock";
+        playerScoreCounter ++;
+    
+    } else if(cpu === "PAPER" ){
+        winner = "CPU won with Paper";
+        cpuScoreCounter ++;
+    }
+}
+
+function resultsPaper(cpu){
+    
+    if(cpu === "ROCK" ){
         winner = "Player won with Paper";
         playerScoreCounter ++;
     }
-    else if( player === "PAPER" && cpu === "SCISSORS" ){
+    else if(cpu === "SCISSORS" ){
         winner = "CPU won with Scissors";
         cpuScoreCounter ++;
     }
-    else if( player === "PAPER" && cpu === "PAPER" ){
+    else if(cpu === "PAPER" ){
         winner = "Draw";
     }
-    else if( player === "ROCK" && cpu === "ROCK" ){
-        winner = "Draw";
-    }
-    else if( player === "ROCK" && cpu === "SCISSORS" ){
-        winner = "Player won with Rock";
-        playerScoreCounter ++;
-    }
-    else if( player === "ROCK" && cpu === "PAPER" ){
-        winner = "CPU won with Paper";
-        cpuScoreCounter ++;
+}
 
-    }
-    else if( player === "SCISSORS" && cpu === "ROCK" ){
+function resultsScissors(cpu){
+
+    if(cpu === "ROCK" ){
         winner = "CPU won with Rock";
         cpuScoreCounter ++;
-    }
-    else if( player === "SCISSORS" && cpu === "SCISSORS" ){
+    } else if(cpu === "SCISSORS" ){
         winner = "Draw";
-    }
-    else if( player === "SCISSORS" && cpu === "PAPER" ){
+     } else if(cpu === "PAPER" ){
         winner = "Player won with Scissors";
         playerScoreCounter ++;
     }
